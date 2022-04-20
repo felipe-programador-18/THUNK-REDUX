67e59ab14f17c236b71b0798a67e43305db93d48
Felipe <machadofelipe2016@outlook.com>
2022-04-20 17:57:20 -0300

    c3-create reducer and adding date

---
+++ thunk/src/reducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+    data: [],
+    isfetching:false
+}
+
+export const reducer = (state= INITIAL_STATE, action) =>{
+   if(action.type === 'LOAD_DATA'){
+
+   }
+   return state
+}
