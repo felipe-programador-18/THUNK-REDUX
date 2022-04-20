b9d1170d69f2c70b798d7372f469f43802d45ab2
Felipe <machadofelipe2016@outlook.com>
2022-04-20 17:57:59 -0300

    c5-connect about redux

---
+++ thunk/src/Info.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+export class Info extends React.Component{
+    render(){
+        return(
+            <span>Testing span Info</span>
+        )
+    }
+}
+
+export default connect() (Info)
