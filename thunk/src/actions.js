b35c92185d4c87e39dbdeddd97cab0e815761c81
Felipe <machadofelipe2016@outlook.com>
2022-04-29 18:03:08 -0300

    c25- trying solve issues

--- thunk/src/actions.js
+++ thunk/src/actions.js
@@ -1,8 +1,17 @@
+//create another const to request date
+//theoritical this function yet
+export const loalRequestDate = () =>{
+    return {
+        type:'LOAD_DATA_REQUEST'
+    }
+}
+
+
 //create actions
 export const loalData= () =>{
-   return {
-       type: 'LOAD_DATA'
-   }
+   return dispatch => {
+   setTimeout(()=> dispatch(loalRequestDate()), 2000)
+   }  
 }
 
 
