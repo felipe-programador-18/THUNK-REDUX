151fa4de87b70ebfafe2e2c39852106aed54097c
Felipe <machadofelipe2016@outlook.com>
2022-05-31 14:36:02 -0300

    c35-adding some things (data, error) in all functions

--- thunk/src/reducer.js
+++ thunk/src/reducer.js
@@ -1,19 +1,28 @@
 
 const INITIAL_STATE = {
     data: [],
-    isFetching: false
+    isFetching: false,
+    error: false
 }
 
 export const reducer = (state= INITIAL_STATE, action) =>{
    if(action.type === 'LOAD_DATA_REQUEST'){
       return {
           isFetching: true,
-          data: []
+          data: [],
+          error: false
       }
    }else if(action.type === 'LOAD_DATA_SUCESS'){
       return{
           isFetching: false,
-          data: action.data
+          data: action.data,
+          error: false
+      }
+   }else if(action.type === 'LOAD_DATA_ERROR'){
+      return{
+          isFetching: false,
+          data: [],
+          error: true
       }
    }
    return state
