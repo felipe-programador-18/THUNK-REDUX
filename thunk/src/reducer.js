be397c4a301f46e59b6525d736d6f048e437e52b
Felipe <machadofelipe2016@outlook.com>
2022-05-31 16:28:11 -0300

    c49-create  back again file

---
+++ thunk/src/reducer.js
@@ -0,0 +1,58 @@
+import  {combineReducers}  from "redux"
+
+const INITIAL_STATE = {
+    data: [],
+    isFetching: false,
+    error: false
+}
+
+//trade name of reducer to up
+ export const ip = (state= INITIAL_STATE, action) =>{
+   if(action.type === 'LOAD_DATA_REQUEST'){
+      return {
+          isFetching: true,
+          data: [],
+          error: false
+      }
+   }else if(action.type === 'LOAD_DATA_SUCESS'){
+      return{
+          isFetching: false,
+          data: action.data,
+          error: false
+      }
+   }else if(action.type === 'LOAD_DATA_ERROR'){
+      return{
+          isFetching: false,
+          data: [],
+          error: true
+      }
+   }
+   return state
+}
+
+
+ export const ua = (state= INITIAL_STATE, action) =>{
+    if(action.type === 'LOAD_UA_REQUEST'){
+       return {
+           isFetching: true,
+           data: [],
+           error: false
+       }
+    }else if(action.type === 'LOAD_UA_SUCESS'){
+       return{
+           isFetching: false,
+           data: action.data,
+           error: false
+       }
+    }else if(action.type === 'LOAD_UA_ERROR'){
+       return{
+           isFetching: false,
+           data: [],
+           error: true
+       }
+    }
+    return state
+ }
+
+
+ export default combineReducers({ip, ua})
