8370c36da405e56fa965c6b541c658a640bf6426
Felipe <machadofelipe2016@outlook.com>
2022-06-18 16:37:45 -0300

    c58-make some testing

--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -1,7 +1,8 @@
-//import  {combineReducers}  from "redux"
-//import ip from './ip'
-//import ua from './ua'
+import  {combineReducers}  from "redux"
 
-//export default combineReducers({ip, ua})
+import ip from './ip'
+import ua from './ua'
+
+export default combineReducers({ip, ua})
 // combine reducer allows create the tiny inside redux!!
 // manage all reducer part, is more beneficial!!
